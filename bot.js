5f05dd23292f7c74ce662e3abaded3436e3dfd67
ahmed86365 <44976294+ahmed86365@users.noreply.github.com>
2018-11-14 20:26:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const client3 = new Discord.Client();
-const client4 = new Discord.Client();
+
 client.on('ready', () => {
    console.log(`----------------`);
    console.log(`Credit Farmm - Script By : Kahrbaa `);
@@ -46,33 +45,6 @@ if (message.content === '!spam') {
 });
 
 
-client3.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-    if (message.content === '!spam') {
-          let count = 0;
-          let ecount = 0;
-          for(let x = 0; x < 90000; x++) {
-            message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-              .then(m => {
-                count++;
-              })
-              
-            }
-          }
-    });
-
-    client4.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-        if (message.content === '!spam') {
-              let count = 0;
-              let ecount = 0;
-              for(let x = 0; x < 90000; x++) {
-                message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-                  .then(m => {
-                    count++;
-                  })
-                  
-                }
-              }
-        });
 
 
 
@@ -116,48 +88,11 @@ client.on('message', message => {
    
   });
 
-  client3.on('message', message => {
-    var prefix = "6";
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-  
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-  
-  
-  let args = message.content.split(" ").slice(1);
-  let x = args.join(" ")
-    if(message.content.startsWith(prefix + 'say')) {
-        message.channel.send(''+x);
-            message.delete(999)
-    }
-    
-   
-  });
 
 
 
 
-  client4.on('message', message => {
-    var prefix = "7";
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-  
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-  
-  
-  let args = message.content.split(" ").slice(1);
-  let x = args.join(" ")
-    if(message.content.startsWith(prefix + 'say')) {
-        message.channel.send(''+x);
-            message.delete(999)
-    }
-    
-   
-  });
+
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
-client3.login(process.env.TOKEN3);// لا تغير فيها شيء
-client4.login(process.env.TOKEN4);// لا تغير فيها شيء
