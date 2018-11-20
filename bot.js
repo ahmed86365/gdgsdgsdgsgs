7f47d19ff0b052a29270dddc08b783c765b1c922
ahmed86365 <44976294+ahmed86365@users.noreply.github.com>
2018-11-20 11:50:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-const prefix = "$"
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '354527650657861633') return;
-
-if (message.content.startsWith(prefix + 'g')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(prefix + 'w')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(prefix + 'l')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(prefix + 's')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/OvErDz1");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-}
-
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`! xVOLLY ³³ ♪ - :heart:`,"http://twitch.tv/OvErDz1")
+client.user.setStatus("dnd")
 });
 
 
 
-
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
